c7da2fc9d11dfca7b1e2494efba76ca071d1bc99
Guilherme Gregorini <guihgo100milha@gmail.com>
2024-12-10 19:44:14 -0300

    [fix] object merge

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,25 +1,20 @@
 /*
 * Recursively merge properties of two objects 
 */
-export function merge(obj1: any, obj2: any) {
+export function merge(target: any, source: any) {
 
-    for (var p in obj2) {
+    for (const key in source) {
         try {
-            // Property in destination object set; update its value.
-            if (obj2[p].constructor == Object) {
-                obj1[p] = merge(obj1[p], obj2[p]);
-
-            } else {
-                obj1[p] = obj2[p];
-
+            if (source[key].constructor == Object) {
+                target[key] = merge(target[key], source[key]);
+            } else if(target[key] === undefined) {
+                target[key] = source[key];
             }
-
         } catch (e) {
-            // Property in destination object not set; create it and set its value.
-            obj1[p] = obj2[p];
+            target[key] = source[key];
 
         }
     }
 
-    return obj1;
+    return target;
 }
